cd100691473377cf10fdcab41cb969e01515bbfa
Jere3y <tianyurui@gmail.com>
2017-05-31 09:37:46 +0800

    优化

--- JereLog.java
+++ JereLog.java
@@ -6,7 +6,7 @@ package jeremy.com.utils;
 
 import android.util.Log;
 
-public class Log {
+public final class Log {
     private Log() {
     }
 
@@ -16,55 +16,55 @@ public class Log {
     private static final boolean DEBUG_D = true;
     private static final boolean DEBUG_V = true;
 
-    public static final void e(String tag, String msg) {
+    public static void e(String tag, String msg) {
         if (DEBUG_E)
             Log.e(tag, msg);
     }
 
-    public static final void e(String tag, String msg, Throwable tr) {
+    public static void e(String tag, String msg, Throwable tr) {
         if (DEBUG_E)
             Log.e(tag, msg, tr);
     }
 
 
-    public static final void w(String tag, String msg) {
+    public static void w(String tag, String msg) {
         if (DEBUG_W)
             Log.w(tag, msg);
     }
 
-    public static final void w(String tag, String msg, Throwable tr) {
+    public static void w(String tag, String msg, Throwable tr) {
         if (DEBUG_W)
             Log.w(tag, msg, tr);
     }
 
 
-    public static final void i(String tag, String msg) {
+    public static void i(String tag, String msg) {
         if (DEBUG_I)
             Log.i(tag, msg);
     }
 
-    public static final void i(String tag, String msg, Throwable tr) {
+    public static void i(String tag, String msg, Throwable tr) {
         if (DEBUG_I)
             Log.i(tag, msg, tr);
     }
 
 
-    public static final void d(String tag, String msg) {
+    public static void d(String tag, String msg) {
         if (DEBUG_D)
             Log.d(tag, msg);
     }
 
-    public static final void d(String tag, String msg, Throwable tr) {
+    public static void d(String tag, String msg, Throwable tr) {
         if (DEBUG_D)
             Log.d(tag, msg, tr);
     }
 
-    public static final void v(String tag, String msg) {
+    public static void v(String tag, String msg) {
         if (DEBUG_V)
             Log.v(tag, msg);
     }
 
-    public static final void v(String tag, String msg, Throwable tr) {
+    public static void v(String tag, String msg, Throwable tr) {
         if (DEBUG_V)
             Log.v(tag, msg, tr);
     }
